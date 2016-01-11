66396432ff785efc83c6471e9d90f2bc230b430f
Will Barton <william.barton@cfpb.gov>
2016-01-11 10:21:06 -0500

    Add support for serializing an edit script as XSL

---
+++ xtdiff/tests/test_xsl.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase, skip
+
+import lxml.etree as etree
+
+from ..diff import (INSERT, UPDATE, MOVE, DELETE, Match)
+from ..xsl import xsldiff
+
+class XDiffXSLTestCase(TestCase):
+
+    def test_toxsl_insert(self):
+        root_one = etree.fromstring("<root></root>")
+        root_two = etree.fromstring("<root><first>A child Node</first></root>")
+        xsl = xsldiff(root_one, root_two)
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+
+    def test_toxsl_update_text(self):
+        root_one = etree.fromstring("<root><first>Some text</first></root>")
+        root_two = etree.fromstring("<root><first>Some text more</first></root>")
+
+        xsl = xsldiff(root_one, root_two)
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+
+    @skip
+    # XXX: We don't handle attribute changes well at all yet.
+    def test_toxsl_update_attributes(self):
+        root_one = etree.fromstring('<root><first attr="old">Some text</first></root>')
+        root_two = etree.fromstring(
+            '<root><first attr="new" and="more">Some text</first></root>')
+
+        xsl = xsldiff(root_one, root_two)
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+    @skip
+    # XXX: We don't handle tail text changes well at all yet.
+    def test_toxsl_update_tail(self):
+        root_one = etree.fromstring("<root><first>Some text</first> more</root>")
+        root_two = etree.fromstring("<root><first>Some text</first> less</root>")
+
+        xsl = xsldiff(root_one, root_two)
+        print(etree.tostring(xsl))
+
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+        print(etree.tostring(result))
+
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+
+    def test_toxsl_move(self):
+        root_one = etree.fromstring("<root><foo>bar</foo><foo>first</foo></root>")
+        root_two = etree.fromstring("<root><foo>first</foo><foo>bar</foo></root>")
+
+        xsl = xsldiff(root_one, root_two)
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+            
+    def test_toxsl_delete(self):
+        root_one = etree.fromstring("<root><foo/></root>")
+        root_two = etree.fromstring("<root></root>")
+
+        xsl = xsldiff(root_one, root_two)
+        transform = etree.XSLT(xsl)
+        result = transform(root_one)
+
+        self.assertEqual(etree.tostring(result),
+                etree.tostring(root_two))
+
+        
+
---
+++ xtdiff/xsl.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Python xtdiff
+
+This implements "Change detection in hierarchically structured
+information", by Sudarshan S. Chawathe, Anand Rajaraman, Hector
+Garcia-Molina, and Jennifer Widom.
+
+Chawathe, Sudarshan S., Anand Rajaraman, Hector Garcia-Molina, and
+Jennifer Widom. "Change detection in hierarchically structured
+information." In ACM SIGMOD Record, vol. 25, no. 2, pp. 493-504. ACM,
+1996.
+"""
+
+from __future__ import unicode_literals
+
+import re
+
+from lxml import etree
+
+from .diff import INSERT, UPDATE, MOVE, DELETE
+from .diff import simplematch, Match, THRESHOLD
+from .diff import diff
+
+
+XSL_NAMESPACE = 'http://www.w3.org/1999/XSL/Transform'
+XSL = '{%s}' % XSL_NAMESPACE
+NSMAP = {'xsl': XSL_NAMESPACE} # the default namespace (no prefix)
+
+
+def insert(action, xsl):
+    """
+    <xsl:template match="/parent">
+        <xsl:copy>
+            <xsl:copy-of select="*[position() < action.index"/>
+            action.node
+            <xsl:copy-of select="*[position() >= action.index"/>
+        </xsl:copy>
+    </xsl:template>
+    """
+    insert = etree.Element(XSL + 'template', nsmap=NSMAP)
+    insert.set('match', action.parent)
+    copy = etree.SubElement(insert, XSL + 'copy', nsmap=NSMAP)
+
+    # Select and keep all the elements that preceed the index where
+    # we're inserting.
+    preceeding_copy_of = etree.SubElement(copy,
+            XSL + 'copy-of', nsmap=NSMAP)
+    preceeding_copy_of.set('select', 
+                           '*[position() < {}]'.format(str(action.index)))
+
+    # Insert the new element
+    copy.append(etree.fromstring(action.node))
+
+    # Select and keep all subsequent elements.
+    succeeding_copy_of = etree.SubElement(copy,
+        XSL + 'copy-of', nsmap=NSMAP)
+    succeeding_copy_of.set('select', 
+                           '*[position() >= {}]'.format(str(action.index)))
+
+    xsl.append(insert)
+
+
+def update(action, xsl):
+    """
+    <xsl:template match="/parent">
+        <xsl:copy>
+            <xsl:copy-of select="*[position() < action.index"/>
+            action.node
+            <xsl:copy-of select="*[position() >= action.index"/>
+        </xsl:copy>
+    </xsl:template>
+    """
+    update = etree.Element(XSL + 'template', nsmap=NSMAP)
+    update.set('match', action.path + '/text()')
+
+    # Update text
+    text = etree.SubElement(update, XSL + 'text', nsmap=NSMAP)
+    text.text = action.text
+
+    # Update attributes
+    # XXX: This is not implemented yet because the matching/script 
+    # code doesn't generate an UDPATE for attribute changes right
+    # now.
+
+    xsl.append(update)
+
+    # Update tail
+    # XXX: This is not implemented yet because tail
+    # matching/actions/etc are just fubar. 
+    # if action.tail:
+    #     update_tail = etree.Element(XSL + 'template', nsmap=NSMAP)
+    #     update_tail.set('match', action.path + '/following-sibling::text()')
+    #     text = etree.SubElement(update_text, XSL + 'text',
+    #             nsmap=NSMAP)
+    #     text.text = action.tail
+    #     xsl.append(update_tail)
+
+
+def move(action, xsl):
+    """
+    <xsl:template match="/node/path"></xsl:template>
+    <xsl:template match="/new/parent">
+        <xsl:copy>
+            <xsl:copy-of select="*[position() < action.index"/>
+            <xsp:copy-of select="/node/path"/>
+            <xsl:copy-of select="*[position() >= action.index"/>
+        </xsl:copy>
+    </xsl:template>
+    """
+    # To move a node basically amounts to a combination of insert
+    # and delete. It is inserted into the parent node and deleted
+    # from its original location. 
+
+    # Find out if the parent already has a template match. If so, we'll
+    # append to that.
+    parent_matches = xsl.xpath('//*[@match="{}"]'.format(action.parent)) 
+    if len(parent_matches) > 0:
+        parent = parent_matches[0]
+    else:
+        parent = etree.SubElement(xsl, XSL + 'template', nsmap=NSMAP)
+
+    parent.set('match', action.parent)
+    insert_copy = etree.SubElement(parent, XSL + 'copy', nsmap=NSMAP)
+
+    # Extract the original position
+    # XXX: Is there a way to make this feel less fragile while not
+    # cluttering up our edit script?
+    index = re.match(r'.*\[([(0-9)+])\]', action.path).groups()[0]
+
+    # Select and keep all the elements that preceed the index where
+    # we're inserting.
+    insert_preceeding_copy_of = etree.SubElement(insert_copy,
+        XSL + 'copy-of', nsmap=NSMAP)
+    insert_preceeding_copy_of.set('select', 
+        '*[position() < {} and position() != {}]'.format(
+            str(action.index + 1), str(index)))
+
+    # Insert the new element
+    insert_copy_of = etree.SubElement(insert_copy,
+        XSL + 'copy-of', nsmap=NSMAP)
+    insert_copy_of.set('select', action.path)
+
+    # Select and keep all subsequent elements.
+    insert_succeeding_copy_of = etree.SubElement(insert_copy,
+        XSL + 'copy-of', nsmap=NSMAP)
+    insert_succeeding_copy_of.set('select', 
+        '*[position() >= {} and  position() != {}]'.format(
+            str(action.index + 1), str(index)))
+
+    delete = etree.Element(XSL + 'template', nsmap=NSMAP)
+    delete.set('match', action.path)
+    xsl.append(delete)
+
+
+def delete(action, xsl):
+    """
+    <xsl:template match="/node/path"></xsl:template>
+    """
+    delete = etree.Element(XSL + 'template', nsmap=NSMAP)
+    delete.set('match', action.path)
+    xsl.append(delete)
+
+
+def toxsl(script, insert=insert, update=update, move=move,
+           delete=delete):
+    ''' Convert the given edit script to an XSL stylesheet.  '''
+
+    # Create the XSL stylesheet
+    xsl = etree.Element(XSL + 'stylesheet', nsmap=NSMAP)
+
+    # Create the identity transformation
+    match_all = etree.SubElement(xsl, XSL + 'template', nsmap=NSMAP)
+    match_all.set('match', '@* | node()')
+    match_all_copy = etree.SubElement(match_all, XSL + 'copy', nsmap=NSMAP)
+    match_all_apply = etree.SubElement(match_all_copy, 
+            XSL + 'apply-templates', nsmap=NSMAP)
+    match_all_apply.set('select', '@* | node()')
+    match_all_apply.set('name', 'identity')
+
+    # Create transformations for each action
+    for action in script:
+        if type(action) == INSERT:
+            insert(action, xsl)
+
+        elif type(action) == UPDATE:
+            update(action, xsl)
+
+        elif type(action) == MOVE:
+            move(action, xsl)
+
+        elif type(action) == DELETE:
+            delete(action, xsl)
+
+    return xsl
+
+
+def xsldiff(left_tree, right_tree, match=simplematch,
+         match_threshold=THRESHOLD):
+    """ Simple wrapper around toxsl(diff()) """
+
+    return toxsl(diff(left_tree, right_tree, match=match,
+                      match_threshold=match_threshold))
